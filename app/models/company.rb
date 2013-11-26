a8f3b211c88b09506fbdc05adb6a2ae053ec6862
emteajay <mtjjoyce@gmail.com>
2013-11-26 15:13:01 -0500

    changes to company and list form

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,18 +2,18 @@ class Company < ActiveRecord::Base
 
 	def self.search_for(query) #self is like company.search_for
 		
-		# our_company = Company.where(query) use when stored to database. DONT DO YET BECAUSE DATABASE WON'T BE UPDATED
+		# our_company = Company.where(name: query) use when stored to database. DONT DO YET BECAUSE DATABASE WON'T BE UPDATED
 		# if our_company.nil?
 
 		return if query.nil?
-		Crunchbase::API.key = 'yecqtsu3dszque7rntydphap'
+		#Crunchbase::API.key = 'yecqtsu3dszque7rntydphap'
 		company = Crunchbase::Company.find(query)
-		our_company = Company.new #create a new instance of the company
-		our_company.name = company.name
-		our_company.total_money_raised = company.total_money_raised
-		our_company.phone_number = company.phone_number
-		our_company.description = company.overview
-		our_company
+		@our_company = Company.new #create a new instance of the company
+		@our_company.name = company.name
+		@our_company.total_money_raised = company.total_money_raised
+		@our_company.phone_number = company.twitter_username
+		@our_company.description = company.overview
+		@our_company
 		# our_company.save #will save to database
 		#puts "Crunchbase company class is #{company.class}" #will only show up in irb
 	
